a605b5428337fcb2fb7c4a1c6695b5cdbae20927
pmelo-ca <pedromelocf@yahoo.com>
2024-08-09 16:44:20 -0300

    improve

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -3,17 +3,17 @@
 import sys
 
 def main():
-	arr = []
 	argc = len(sys.argv) - 1
-	begin = int(sys.argv[1])
-	end = int(sys.argv[2]) + 1
 
 	if argc == 2:
-		for begin in range(begin, end):
-			arr.append(begin)
+
+		begin = int(sys.argv[1])
+		end = int(sys.argv[2]) + 1
+
+		arr = [begin for begin in range(begin, end)]
+		
 		print(arr)
 	else:
 		print("none")
 
-if __name__ == "__main__":
-	main()
+main()
